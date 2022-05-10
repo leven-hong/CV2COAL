d4d7685b4aa531808353e79f29b2db6b02bbc0f7
hetun <leven008@outlook.com>
2022-05-10 18:58:19 +0800

    完美显示与计算三通道

--- code/main.py
+++ code/main.py
@@ -1,9 +1,35 @@
+from asyncio import shield
+
+from matplotlib.pyplot import axis
 from function import *
+from tqdm import tqdm
+import numpy as np
 
-hist=hist_cal('img/1.PNG')
 
-hist_save(hist,'1.jpg')
-dHist=d_hist(hist)
-hist_save(dHist,'2.jpg')
-sHist=s_hist(hist)
-hist_save(sHist,'3.jpg')
+def main(n):
+    histAll=[]
+    dHistAll=[]
+    sHistAll=[]
+    for i in tqdm(range(n)):
+        hist=hist_cal('img/'+str(i)+'.PNG')
+        histAll.append(hist)
+        hist_save(hist,'hist/'+str(i)+'.jpg')
+
+        dHist=d_hist(hist)
+        dHistAll.append(dHist)
+        hist_save(dHist,'hist_d/'+str(i)+'.jpg')
+
+        sHist=s_hist(hist)
+        sHistAll.append(sHist)
+        hist_save(sHist,'hist_s/'+str(i)+'.jpg')
+    histSum=np.sum(histAll,axis=0)
+    hist_save(histSum,'HIST.jpg')
+    dHistSum=np.sum(dHistAll,axis=0)
+    hist_save(dHistSum,'HIST_d.jpg')
+    sHistSum=np.sum(sHistAll,axis=0)
+    hist_save(sHistSum,'HIST_s.jpg')
+
+    
+
+
+main(75)
